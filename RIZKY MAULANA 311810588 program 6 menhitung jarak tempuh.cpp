3766b38afb0d9b3882dfa01d3014878e4f3b8419
rizkymaulana803 <46336552+rizkymaulana803@users.noreply.github.com>
2019-01-03 19:43:17 +0700

    Add files via upload

---
+++ RIZKY MAULANA 311810588 program 6 menhitung jarak tempuh.cpp
@@ -0,0 +1,22 @@
+	#include <iostream>
+	using namespace std;
+	
+	int main()
+	{
+		int v,t,s;
+		
+		cout <<"---Menghitung Jarak Temouh Perjalanan---";
+	
+		
+		cout <<"/nMasukan Kecepatan M/Detik= ";
+		cin >> v;
+		
+		cout <<"nMasukan Waktu yang di Tempuh (detik) = ";
+		cin >> t;
+		
+		//Rumus
+		s = v * t;
+		cout <<"Jarak yang di Tempuh adalah = "<<5<<"Meter";
+		
+		return 0;
+	}
